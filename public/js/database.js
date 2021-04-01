746049c8fb1039265cf9331ea1d027d51436d636
Dominik <dominikborek@seznam.cz>
2021-04-01 17:34:24 +0200

    database

--- public/js/database.js
+++ public/js/database.js
@@ -2,7 +2,7 @@ const mysql = require ('mysql');
 const dbConnection = mysql.createPool({
     host        :   'localhost',
     user        :   'root',
-    password    :   '';
+    password    :   '',
     database    :   ''
 }).promise();
 module.exports = dbConnection
