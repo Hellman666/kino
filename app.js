a387084f79c0ee58c709b719b0f12299eb1061a3
Dominik <dominikborek@seznam.cz>
2021-04-13 23:08:20 +0200

    finish login

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
-// zapnutí aplikace: npm start
-//imports
 const express = require('express')
 const bodyParser = require('body-parser')
-const mysql = require('mysql')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const connectFlash = require('connect-flash')
+const passport = require('passport')
+
+import configViewEngine from "./configs/viewEngine";
+import initWebRoutes from "./routes/web";
 
 const app = express()
-const port = 3000
 
-/*app.use(bodyParser.urlencoded());
+//use cookie parser
+app.use(cookieParser('secret'));
+//config session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 // 86400000 1 day
+    }
+}));
+// Enable body parser post data
 app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({
-    extended: true
-  }));*/
-
-const pool = mysql.createPool({
-    connectionLimit : 10,
-    host            : 'localhost',
-    user            : 'root',
-    password        : '',
-    database        : 'kina'
-});
-//static files
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/img', express.static(__dirname + 'public/img'))
-
-//set views
-app.set('views', './views');
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+//Config view engine
+configViewEngine(app);
+//Enable flash message;
+app.use(connectFlash());
+//Config passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+// init all web routes
+initWebRoutes(app);
 
 app.get('/index', (req, res) => {
     res.render('index', { Text: 'Kino Hvězda' })
@@ -40,123 +43,17 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/films', (req, res) => {
-    res.render('films', { Text: 'Filmy' }),
-    connection.query('SELECT * FROM filmy', function (err, film, fields){
-        if(err) throw err 
-        
-            res.render('films', {title: 'Films Details',
-            items: film })
-    })
-    //res.console(rows)
+    res.render('films', { Text: 'Filmy' })
 })
 
 app.get('/actors', (req, res) => {
     res.render('actors', { Text: 'Herci' })
-    /*pool.getConnection((err, connection) => {
-        if(err) throw err
-        console.log('connected as id ${connection.threadId}')
-        */
-        connection.query('SELECT * FROM herci', (err, herec) =>{
-            if(err) throw err
-
-            /*connection.release()
-            if(!err){
-                res.send(herec)
-                //console.log(herec)
-            } else {
-                console.log(err)
-            }
-        })*/
-        res.render('actors', {title: 'Actors Details',
-        items: 'herec'})
-    })
 })
 
 app.get('/projections', (req, res) => {
     res.render('projections', { Text: 'Projekce' })
-
-   /* pool.getConnection((err, connection) => {
-        if(err) throw err
-        console.log('connected as id ${connection.threadId}')
-*/
-        connection.query('SELECT * FROM promitani', (err, projekce) =>{
-            if (err) throw err
-            res.render('projections', {title: 'Projections Details',
-            items: 'projekce'})
-            /*connection.release()
-            if(!err){
-                res.send(projekce)
-                //console.log(projekce)
-            } else {
-                console.log(err)
-            }
-        })*/
-    })
-})
-
-app.get('/register', (req, res) => {
-    res.render('register', { Text: 'Registrace' })
-})
-
-app.get('/login', (req, res) => {
-    res.render('login', { Text: 'Přihlášení' })
-})
-
-/*app.get('', (req, res) =>{
-    res.sendFile(__dirname + '/views/index.html')
-})*/
-
-
-
-
-//Get all 
-app.get('', (req, res) => {
-
-    pool.getConnection((err, connection) => {
-        if(err) throw err 
-        console.log('connected as id ${connection.threadId}')
-        
-        // query(sqlString, callback)
-        connection.query('SELECT * from filmy', (err, rows) =>{
-            connection.release() //return the connection to pool
-            
-            if(!err) {
-                res.send(rows)
-                //console.log(rows)
-            } else {
-                console.log(err)
-            }
-        })
-    })
-
 })
 
 
-
-//get by id
-/*app.get('', (req, res) => {
-
-    pool.getConnection((err, connection) => {
-        if(err) throw err 
-        console.log('connected as id ${connection.threadId}')
-
-        // query(sqlString, callback)
-        connection.query('SELECT * from herci ORDER BY idherci', (err, rows) =>{
-            connection.release() //return the connection to pool
-
-            if(!err) {
-                res.send(rows)
-            } else {
-                console.log(err)
-            }
-        })
-    })
-
-})
-*/
-
-/*app.use('public')
-app.use("/bootstrap", express.static(__dirname, 'public/css/bootstrap/dist/css/bootstrap.css'));*/
-
-// Listen on port 3000
-app.listen(port, () => console.info(`App listening on port ${port}`))
+let port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Building a login system with NodeJS is running on port ${port}!`));
